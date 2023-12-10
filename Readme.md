3a3553126a763fb52240bf988cee04ad62b232fe
rafaelgarcia <rafa@myariadna.com>
2023-12-10 14:10:22 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# nodejs-typescript
+## General pourpose
+Back bone for create new nodejs applications with type script
+## Funtionalities
+Rest API based on Express accesing a MySQL database 
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from Express Typescript');
+})
+
+app.listen(port, ()=> {
+    console.log(`Server runnig at port: ${port}`)
+})
+
